77353c27c29ca9d2bd588d594bb3b38d53fae022
Wellington Feitoza <spaaws@gmail.com>
2022-04-16 14:33:33 -0300

    implementação api

---
+++ main.go
@@ -0,0 +1,53 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+type Event struct {
+	Id          int    `json:"id"`
+	Title       string `json:"title"`
+	Descritpion string `json:"description"`
+}
+
+type allEvents []Event
+
+var events = allEvents{
+	{
+		Id:          1,
+		Title:       "APIs",
+		Descritpion: "Trocando conhecimento com a turma da pós-graduação.",
+	},
+}
+
+func main() {
+	log.Println("Starting API")
+	router := mux.NewRouter()
+	router.HandleFunc("/", Home)
+	router.HandleFunc("/health-check", HealthCheck).Methods("GET")
+	router.HandleFunc("/events", GetAllEvents).Methods("GET")
+
+	http.ListenAndServe(":8080", router)
+}
+
+func Home(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Aplicação em execução!\n")
+}
+
+func HealthCheck(w http.ResponseWriter, r *http.Request) {
+	log.Println("acessando health-check")
+	w.WriteHeader(http.StatusOK)
+	fmt.Fprintf(w, "Aplicação Funcionando...\n")
+}
+
+func GetAllEvents(w http.ResponseWriter, r *http.Request) {
+	log.Println("acessando o endpoint get all events")
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	json.NewEncoder(w).Encode(events)
+}
