1fdae0b2fdf98b0e57821c3521f6e224715211e8
Wellington Feitoza <spaaws@gmail.com>
2022-04-16 16:09:03 -0300

    implementação doc swagger + create new event

--- go.mod
+++ go.mod
@@ -2,4 +2,28 @@ module events
 
 go 1.18
 
-require github.com/gorilla/mux v1.8.0 // indirect
+require (
+	github.com/KyleBanks/depth v1.2.1 // indirect
+	github.com/PuerkitoBio/purell v1.1.1 // indirect
+	github.com/PuerkitoBio/urlesc v0.0.0-20170810143723-de5bf2ad4578 // indirect
+	github.com/cpuguy83/go-md2man/v2 v2.0.1 // indirect
+	github.com/ghodss/yaml v1.0.0 // indirect
+	github.com/go-openapi/jsonpointer v0.19.5 // indirect
+	github.com/go-openapi/jsonreference v0.20.0 // indirect
+	github.com/go-openapi/spec v0.20.5 // indirect
+	github.com/go-openapi/swag v0.21.1 // indirect
+	github.com/gofrs/uuid v4.2.0+incompatible // indirect
+	github.com/gorilla/mux v1.8.0 // indirect
+	github.com/josharian/intern v1.0.0 // indirect
+	github.com/mailru/easyjson v0.7.7 // indirect
+	github.com/russross/blackfriday/v2 v2.1.0 // indirect
+	github.com/satori/go.uuid v1.2.0 // indirect
+	github.com/shurcooL/sanitized_anchor_name v1.0.0 // indirect
+	github.com/swaggo/swag v1.8.1 // indirect
+	github.com/urfave/cli/v2 v2.4.0 // indirect
+	golang.org/x/net v0.0.0-20220412020605-290c469a71a5 // indirect
+	golang.org/x/sys v0.0.0-20220412211240-33da011f77ad // indirect
+	golang.org/x/text v0.3.7 // indirect
+	golang.org/x/tools v0.1.10 // indirect
+	gopkg.in/yaml.v2 v2.4.0 // indirect
+)
--- main.go
+++ main.go
@@ -3,29 +3,38 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"io/ioutil"
 	"log"
 	"net/http"
 	"os"
 
+	"github.com/gofrs/uuid"
 	"github.com/gorilla/mux"
 )
 
 type Event struct {
-	Id          int    `json:"id"`
-	Title       string `json:"title"`
-	Descritpion string `json:"description"`
+	Id          uuid.UUID `json:"id"`
+	Title       string    `json:"title"`
+	Descritpion string    `json:"description"`
 }
 
 type allEvents []Event
 
-var events = allEvents{
-	{
-		Id:          1,
-		Title:       "APIs",
-		Descritpion: "Trocando conhecimento com a turma da pós-graduação.",
-	},
-}
+var events = allEvents{}
 
+// @title           Swagger API-Event
+// @version         1.0
+// @description     Documentação da API de eventos.
+// @termsOfService  http://swagger.io/terms/
+
+// @contact.name   API-Event Support
+// @contact.url    http://www.swagger.io/support
+// @contact.email  support@swagger.io
+
+// @license.name  Apache 2.0
+// @license.url   http://www.apache.org/licenses/LICENSE-2.0.html
+
+// @host      https://new-api-events.herokuapp.com/
 func main() {
 	log.Println("Starting API")
 	port := os.Getenv("PORT")
@@ -33,8 +42,10 @@ func main() {
 	router.HandleFunc("/", Home)
 	router.HandleFunc("/health-check", HealthCheck).Methods("GET")
 	router.HandleFunc("/events", GetAllEvents).Methods("GET")
+	router.HandleFunc("/event", CreateNewEvent).Methods("POST")
 
 	http.ListenAndServe(":"+port, router)
+	// http.ListenAndServe(":8080", router)
 }
 
 func Home(w http.ResponseWriter, r *http.Request) {
@@ -47,9 +58,41 @@ func HealthCheck(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Aplicação Funcionando...\n")
 }
 
+// ShowAllEvents godoc
+// @Summary      Show all events
+// @Description  List all events
+// @Tags         events
+// @Accept       json
+// @Produce      json
+// @Success      200  {object}  Event
+// @Router       /events [get]
 func GetAllEvents(w http.ResponseWriter, r *http.Request) {
 	log.Println("acessando o endpoint get all events")
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(http.StatusOK)
 	json.NewEncoder(w).Encode(events)
+}
+
+// CreateNewEvent godoc
+// @Summary      New event
+// @Description  Creater new events
+// @Tags         events
+// @Accept       json
+// @Produce      json
+// @Param        id    title    description    "Event"
+// @Succees      201  {object}  Event
+// @Router       /event [post]
+func CreateNewEvent(w http.ResponseWriter, r *http.Request) {
+	log.Println("acessando o endpoint create new event")
+	var newEvent Event
+
+	newEvent.Id, _ = uuid.NewV4()
+
+	reqBody, _ := ioutil.ReadAll(r.Body)
+	json.Unmarshal(reqBody, &newEvent)
+
+	events = append(events, newEvent)
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusCreated)
+	json.NewEncoder(w).Encode(newEvent)
 }
